ce77a11581d68123ce07fa74d2164eca6eb6a83e
심재성 <simsorry@simjaeseong-ui-MacBookAir.local>
2024-03-13 20:35:48 +0900

    readme 수정:

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
+import {Container, Nav, Navbar, Row, Col} from 'react-bootstrap';
+import { useState } from 'react';
+import data from './data.js';
+import Detail from './routes/detail.js';
+import { Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom';
 
 function App() {
+  let [shoes] = useState(data);
+  let navigate = useNavigate();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="light" data-bs-theme="light">
+        <Container>
+          <Navbar.Brand href="#home">SorryShop</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link onClick={()=>navigate('/')} href="#home">Home</Nav.Link>
+            <Nav.Link onClick={()=>navigate('/detail')} href="#features">Cart</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <div className='main-bg'/>
+      <Routes>
+        <Route path="/" element={
+          <>
+           <Container>
+            <Row>
+              {
+                shoes.map(function(a,i){ 
+                  return(
+                    <Shoes a = {a} i = {i + 1} navigate={navigate}/>
+                  )
+                })
+              }
+            </Row>
+          </Container>
+          </>
+        }/>
+        <Route path="/detail" element={<Detail/>}/>
+        <Route path="*" element={<div>404</div>}/>
+      </Routes>
+   
     </div>
   );
+}
+
+function Shoes(props){
+  return(
+    <Col>
+      <img src={"https://codingapple1.github.io/shop/shoes" +props.i+ ".jpg"}
+        style={{width:'80%'}}>
+      </img>
+      <h5 onClick={()=>{props.navigate('/detail')}}>{props.a.title}</h5>
+      <p>{props.a.content}</p>
+    </Col>
+  )
 }
 
 export default App;
