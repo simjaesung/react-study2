6157a5fc067e1c4a94dc4d20277f3c2b193787a9
심재성 <simsorry@simjaeseong-ui-MacBookAir.local>
2024-03-13 22:30:57 +0900

    react 쇼핑몰 프로젝트 ~ params

--- src/App.js
+++ src/App.js
@@ -12,10 +12,12 @@ function App() {
     <div className="App">
       <Navbar bg="light" data-bs-theme="light">
         <Container>
-          <Navbar.Brand href="#home">SorryShop</Navbar.Brand>
+          <Navbar.Brand href="#home">
+            <Nav.Link onClick={()=>navigate('/')}>SorryShop</Nav.Link>
+          </Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link onClick={()=>navigate('/')} href="#home">Home</Nav.Link>
-            <Nav.Link onClick={()=>navigate('/detail')} href="#features">Cart</Nav.Link>
+            <Nav.Link onClick={()=>navigate('/')}>Home</Nav.Link>
+            <Nav.Link onClick={()=>navigate('/detail')}>Cart</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
@@ -28,7 +30,7 @@ function App() {
               {
                 shoes.map(function(a,i){ 
                   return(
-                    <Shoes a = {a} i = {i + 1} navigate={navigate}/>
+                    <Shoes a = {a} i = {i+1} navigate={navigate}/>
                   )
                 })
               }
@@ -36,8 +38,18 @@ function App() {
           </Container>
           </>
         }/>
-        <Route path="/detail" element={<Detail/>}/>
-        <Route path="*" element={<div>404</div>}/>
+        <Route path="/detail/:id" element={<Detail shoes={shoes}/>}/>
+
+        <Route path="/about" element={<About/>}>
+          <Route path="member" element ={<div>멤버페이지</div>}/>
+          <Route path="location" element={<div>지역페이지</div>}/>
+        </Route>
+        <Route path="/event" element={<><h3>오늘의 이벤트</h3> <Outlet></Outlet></>}>
+          <Route path="one" element={<h5>첫 주문시 양배추즙 서비스</h5>}/>
+          <Route  path="two" element={<h5>생일기념 쿠폰 받기</h5>}/>
+        </Route>
+
+        <Route path="*" element={<div>잘못된 페이지입니다.</div>}/>
       </Routes>
    
     </div>
@@ -50,9 +62,18 @@ function Shoes(props){
       <img src={"https://codingapple1.github.io/shop/shoes" +props.i+ ".jpg"}
         style={{width:'80%'}}>
       </img>
-      <h5 onClick={()=>{props.navigate('/detail')}}>{props.a.title}</h5>
+      <h5 onClick={()=>{props.navigate('/detail/'+ props.a.id)}}>{props.a.title}</h5>
       <p>{props.a.content}</p>
     </Col>
+  )
+}
+
+function About(){
+  return (
+    <>
+    <div>어바웃페이지</div>
+    <Outlet></Outlet>
+    </>
   )
 }
 
--- src/routes/detail.js
+++ src/routes/detail.js
@@ -1,14 +1,28 @@
-function Detail(){
+import { useParams } from "react-router-dom";
+
+function Detail(props){
+    let {id} = useParams(); 
+    let s = 0;
+    console.log(id);
+    for(let i = 0; i<props.shoes.length; i++){
+        if(props.shoes[i].id == id) s = i;
+    }
+    
+    let s2 = props.shoes.find(function(x){
+        return x.id == id;
+    })
+
+
     return(
         <div className="container">
             <div className="row">
                 <div className="col-md-6">
-                <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
+                <img src={"https://codingapple1.github.io/shop/shoes"+(++s)+".jpg"} width="100%" />
                 </div>
                 <div className="col-md-6">
-                <h4 className="pt-5">상품명</h4>
-                <p>상품설명</p>
-                <p>120000원</p>
+                <h4 className="pt-5">{props.shoes[s-1].title}</h4>
+                <p>{props.shoes[s-1].content}</p>
+                <p>{props.shoes[s-1].price}원</p>
                 <button className="btn btn-danger">주문하기</button> 
                 </div>
             </div>
